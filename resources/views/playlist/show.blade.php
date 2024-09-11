31ca9aaf0b7dcd109041be658990a8a34081f59c
unknown <your@email.ex>
2024-09-11 16:31:48 +0300

    first commit

---
+++ resources/views/playlist/show.blade.php
@@ -0,0 +1,53 @@
+<x-app-layout>
+    <div class="w-full rounded overflow-hidden shadow-lg p-4 bg-white mb-4">
+            <div class="flex justify-between">
+                <div>       
+                    <a class="font-bold text-xl mb-2" href="{{ route('playlist.show', $playlist->id) }}">
+                        {{ $playlist->name }}
+                    </a>
+                    <div class="px-6 pt-4 pb-2">
+                        <span class="inline-block shadow-lg bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ $playlist->tag }}</span>
+                    </div>
+                </div>
+                <div>
+                    <a href="{{ route('playlist.edit', $playlist->id) }}" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mr-2">
+                        Edit
+                    </a>
+                    <form action="{{ route('playlist.destroy', $playlist->id) }}" method="POST" class="inline-block">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                            Delete
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <table class="w-full table-auto">
+                    <tbody>
+                        <tr>
+                        @foreach ($playlist->songs as $song)
+                            <tr>
+                                <!-- song
+                                <td class="border px-4 py-2">{{$song->title}}</td>
+                                <td class="border px-4 py-2">{{$song->artist}}</td>
+                                <td class="border px-4 py-2">{{$song->genre}}</td> -->
+
+                                <form action="/action_page.php">
+  <label for="cars">Choose a car:</label>
+  <select name="cars" id="cars">
+    <option value="volvo">BMW</option>
+  </select>
+  <br><br>
+</form>
+
+                                
+                            </tr>
+                            @endforeach
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
